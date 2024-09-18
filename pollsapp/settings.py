7cbe2e8c07bea363075694a5ff42465ea0036a53
webxazim <webxazim@gmail.com>
2024-09-18 19:33:37 +0300

    2nd push

--- pollsapp/settings.py
+++ pollsapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(y(xp&tm%q#)0t)i%=e8q+80nhm^+$$#uha8b%fk=7j=q=l1k!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
